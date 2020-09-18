910ef02086fc330030ef5509356c932849d78038
jayelle0 <you@example.com>
2020-09-17 22:02:25 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+
+require 'pry'
+
+
+
+class School 
+
+    attr_accessor :name, :grade
+    attr_reader :roster
+
+    def initialize (name) 
+        @name = name
+        @roster = {}
+   
+    end 
+
+
+    def add_student(student_name, grade)
+        roster[grade] ||= []
+        roster[grade] << student_name
+    end
+
+    def grade(this_grade)
+        roster[this_grade]
+    end 
+
+    def sort
+        sorted = {}
+        roster.each do |grade, students|
+        sorted[grade] = students.sort
+        end
+        sorted
+        end 
+
+end 
